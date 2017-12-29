3a11e60466e5a5166c3028108812c34fb40592cd
Winnie Luk <winniecluk@gmail.com>
2017-12-28 22:41:27 -0800

    initial commit

---
+++ script-jquery.js
@@ -0,0 +1,111 @@
+var setTotal = 10;
+var totalQuarters = 10;
+var costPerTurn = 1;
+var numberOfRectangles = 3;
+var amountPerWinnings = 5;
+var numberOfRectangles = 3;
+var musicRef = {
+  'win': 'sounds/win.wav'
+  , 'loss': 'sounds/loss.wav'
+  , 'spin': 'sounds/spin.wav'
+}
+var imagesRef = {
+  "lemon": {
+    value: 0
+    , imagePath: "images/lemon.png"
+  }
+  , "seven": {
+    value: 1
+    , imagePath: "images/seven.png"
+  }
+  , "watermelon": {
+    value: 2
+    , imagePath: "images/watermelon.png"
+  }
+}
+
+startGame();
+function startGame(){
+  totalQuarters = setTotal;
+  renderRectangles();
+  displayQuarters(null);
+}
+
+function renderRectangles(){
+  var slotMachine = $('.slot-machine');
+  slotMachine.empty();
+  for (let i = 0; i < numberOfRectangles; i++){
+    // see three rectangles next to each other w/ an image
+    var randomSlot = getRandomSlot();
+    createSlotSpan(randomSlot, $('.slot-machine'));
+  }
+}
+
+function getRandomSlot(){
+  var arrKeys = Object.keys(imagesRef);
+  var randomIdx = Math.floor(Math.random() * arrKeys.length);
+  var randomKey = arrKeys[randomIdx];
+  return imagesRef[randomKey];
+}
+
+function createSlotSpan(randomSlot, parent){
+  var imagePath = randomSlot.imagePath;
+  var span = $('<span><img src=' + imagePath + ' /></span>')
+  parent.append(span);
+}
+
+function createAudio(key){
+  var audioElement = $('<audio src=' + musicRef[key] + '/>');
+  audioElement.trigger('play');
+}
+
+function displayQuarters(str, amount){
+  if (str === 'plus') totalQuarters += amount;
+  else if (str === 'minus') totalQuarters -= amount;
+  $('span.quarters-display').text(totalQuarters);
+}
+
+$('button.lever').on('click', function(e){
+  displayQuarters('minus', costPerTurn);
+  var value = 0;
+  var loss = false;
+  var slotMachine = $('.slot-machine');
+  slotMachine.empty();
+  for (let i = 0; i < numberOfRectangles; i++){
+    var randomSlot = getRandomSlot();
+    createSlotSpan(randomSlot, $('.slot-machine'));
+    var valueOfSlot = randomSlot.value;
+    if (loss) continue;
+    if (i === 0){
+      value = valueOfSlot;
+    } else if (valueOfSlot != value){
+      loss = true;
+    }
+  } // closes for loop
+  setTimeout(function(){
+    if (totalQuarters === 0){
+      createAudio('loss');
+      document.querySelector('button.lever').setAttribute('disabled', 'true');
+      createMessage('show');
+      // display message
+    } else if (!loss){
+      displayQuarters('plus', amountPerWinnings);
+      createAudio('win');
+    } else {
+      createAudio('spin');
+    }
+  }, 200);
+});
+
+function createMessage(display){
+  if (display === 'show') $('div.message').removeClass('hide');
+  else if (display === 'hide') $('div.message').addClass('hide');
+  $('div.message-text').text('You are out of quarters! Replay?');
+  $('div.options').html('<button id="replay">Yes</button>');
+}
+
+$('body').on('click', '#replay', function(e){
+    createMessage('hide');
+    $('button.lever').removeAttr('disabled');
+    startGame();
+});
---
+++ script.js
@@ -0,0 +1,118 @@
+// set total of quarters
+var setTotal = 10;
+var totalQuarters = setTotal;
+var costPerTurn = 1;
+var amountPerWinnings = 5;
+var numberOfRectangles = 3;
+var musicRef = {
+  'win': 'sounds/win.wav'
+  , 'loss': 'sounds/loss.wav'
+  , 'spin': 'sounds/spin.wav'
+}
+var imagesRef = {
+  'lemon': {
+    value: 0
+    , imagePath: 'images/lemon.png'
+  }
+  , 'seven': {
+    value: 1
+    , imagePath: 'images/seven.png'
+  }
+  , 'watermelon': {
+    value: 2
+    , imagePath: 'images/watermelon.png'
+  }
+}
+
+startGame();
+function startGame(){
+  totalQuarters = setTotal;
+  renderRectangles();
+  displayQuarters(null);
+}
+
+// see three rectangles next to each other w/ an image
+function renderRectangles(){
+  document.querySelector('.slot-machine').innerHTML = '';
+  for (let i = 0; i < numberOfRectangles; i++){
+    var randomSlot = getRandomSlot();
+    createSlotSpan(randomSlot, document.querySelector('.slot-machine'));
+  }
+}
+
+function getRandomSlot(){
+  var arrKeys = Object.keys(imagesRef);
+  var randomIdx = Math.floor(Math.random() * arrKeys.length);
+  var randomKey = arrKeys[randomIdx];
+  return imagesRef[randomKey];
+}
+
+function createSlotSpan(randomSlot, parent){
+  var imagePath = randomSlot.imagePath;
+  var span = document.createElement('span');
+  span.innerHTML = '<img src=' + imagePath + ' />';
+  parent.appendChild(span);
+}
+
+function createAudio(win){
+  var audioElement = document.createElement('audio');
+  audioElement.src = musicRef[win];
+  audioElement.play();
+}
+
+function displayQuarters(str, amount){
+  if (str === 'plus') totalQuarters += amount;
+  else if (str === 'minus') totalQuarters -= amount;
+  document.querySelector('span.quarters-display').textContent = totalQuarters;
+}
+
+// pull the handle
+document.querySelector('button.lever').addEventListener('click', function(e){
+  // put a quarter in, quarter should be deducted from total
+  displayQuarters('minus', costPerTurn);
+  // images on the three rectangles spins and music plays
+  var value = 0;
+  var loss = false;
+  document.querySelector('.slot-machine').innerHTML = '';
+  for (let i = 0; i < numberOfRectangles; i++){
+    var randomSlot = getRandomSlot();
+    createSlotSpan(randomSlot, document.querySelector('.slot-machine'));
+    var valueOfSlot = randomSlot.value;
+    if (loss) continue;
+    if (i === 0){
+      value = valueOfSlot;
+    } else if (valueOfSlot != value){
+      loss = true;
+    }
+  } // closes for loop
+  setTimeout(function(){
+    // if there is match, we win more quarters and play again
+    // if we are out of quarters, we cannot play
+      if (totalQuarters === 0){
+        createAudio('loss');
+        document.querySelector('button.lever').setAttribute('disabled', 'true');
+        createMessage('show');
+        // display message
+      } else if (!loss){
+        displayQuarters('plus', amountPerWinnings);
+        createAudio('win');
+      } else {
+        createAudio('spin');
+      }
+  }, 200);
+});
+
+function createMessage(display){
+  if (display === 'show') document.querySelector('div.message').classList.remove('hide');
+  else if (display === 'hide') document.querySelector('div.message').classList.add('hide');
+  document.querySelector('div.message-text').textContent = 'You are out of quarters! Replay?';
+  document.querySelector('div.options').innerHTML = '<button id="replay">Yes</button>';
+}
+
+document.addEventListener('click', function(e){
+  if (e.target.id === 'replay'){
+    createMessage('hide');
+    document.querySelector('button.lever').removeAttribute('disabled');
+    startGame();
+  };
+})
